2c32499e26016246f129c20743d3caacd6771010
Amar Phule <amardeepphule@gmail.com>
2024-07-08 22:51:31 +0530

    installed tailwindcss, react-router-dom

--- my-shop-tanstack/src/App.jsx
+++ my-shop-tanstack/src/App.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>My shop</h1>
+      <h1 className="text-4xl font-semibold underline text-center py-5 mt-3">
+        Today's Purchase
+      </h1>
     </>
   );
 }
